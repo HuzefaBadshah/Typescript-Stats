1c73492dd2d9e16fbf39a3e22422aacb446dc02c
IMPETUS\huzefa.badshah <huzefa.badshah@impetus.com>
2023-06-25 11:25:03 +0530

    add tuple MatchData in CsvFileReader.ts

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,21 @@
 // to use any library inside node standard library with typescript install type definition:
 // npm install @types/node
-import fs from "fs";
+import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult";
 
-const matches = fs
-  .readFileSync(`${__dirname}/../football.csv`, {
-    encoding: "utf-8",
-  })
-  .split("\n")
-  .map((item: string): string[] => item.split(","));
+// const matches = fs
+//   .readFileSync(`${__dirname}/../football.csv`, {
+//     encoding: "utf-8",
+//   })
+//   .split("\n")
+//   .map((item: string): string[] => item.split(","));
 
-console.log("match is =============> ", matches);
+const reader = new CsvFileReader("football.csv");
+reader.read();
 
-// enum - enumeration
-enum MatchResult {
-  HomeWin = "H",
-  AwayWin = "A",
-  Draw = "D",
-}
+console.log("match is =============> ", reader.data);
 
-const count = matches.reduce((acc: number, match: string[]): number => {
+const count = reader.data.reduce((acc: number, match): number => {
   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     return ++acc;
   } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
