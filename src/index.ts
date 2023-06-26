80547ea7c9658e101fec7fcceb7bbc88bfffed42
IMPETUS\huzefa.badshah <huzefa.badshah@impetus.com>
2023-06-26 23:19:49 +0530

    done implementing alternate solution to Generics

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,16 @@
 // to use any library inside node standard library with typescript install type definition:
 // npm install @types/node
 import { CsvFileReader } from "./CsvFileReader";
+import { MatchReader } from "./MatchReader";
 import { MatchResult } from "./MatchResult";
 
-// const matches = fs
-//   .readFileSync(`${__dirname}/../football.csv`, {
-//     encoding: "utf-8",
-//   })
-//   .split("\n")
-//   .map((item: string): string[] => item.split(","));
+const csvFileReader = new CsvFileReader("football.csv");
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
-const reader = new CsvFileReader("football.csv");
-reader.read();
+console.log("match is =============> ", matchReader.matches);
 
-console.log("match is =============> ", reader.data);
-
-const count = reader.data.reduce((acc: number, match): number => {
+const count = matchReader.matches.reduce((acc: number, match): number => {
   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     return ++acc;
   } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
